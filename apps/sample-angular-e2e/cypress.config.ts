2d38f12c28e58f2986365026f6cd1278ebd88bda
Nguyen Vuong Huy (MS/EMN42) <unh3hc@bosch.com>
2024-11-26 18:09:12 +0700

    first

---
+++ apps/sample-angular-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run sample-angular:serve',
+        production: 'npx nx run sample-angular:serve-static',
+      },
+      ciWebServerCommand: 'npx nx run sample-angular:serve-static',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
