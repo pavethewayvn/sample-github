63ee7e5e6c152c35a826bdf679356b6ebe3963e6
Nguyen Vuong Huy (MS/EMN42) <unh3hc@bosch.com>
2024-11-26 22:21:52 +0700

    add

--- apps/sample-angular-e2e/cypress.config.ts
+++ apps/sample-angular-e2e/cypress.config.ts
@@ -1,18 +1,38 @@
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+// import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
+// import { defineConfig } from 'cypress';
+
+// export default defineConfig({
+//   e2e: {
+//     ...nxE2EPreset(__filename, {
+//       cypressDir: 'src',
+//       webServerCommands: {
+//         default: 'npx nx run sample-angular:serve',
+//         production: 'npx nx run sample-angular:serve-static',
+//       },
+//       ciWebServerCommand: 'npx nx run sample-angular:serve-static',
+//       ciBaseUrl: 'http://localhost:4200',
+//     }),
+//     baseUrl: 'http://localhost:4200',
+//   },
+// });
+
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
+
+const preset = nxE2EPreset(__filename, {
+  cypressDir: 'src',
+  webServerCommands: {
+    default: 'npx nx run sample-angular:serve',
+    production: 'npx nx run sample-angular:serve-static',
+  },
+  ciWebServerCommand: 'npx nx run sample-angular:serve-static',
+  ciBaseUrl: 'http://localhost:4200',
+});
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
-      webServerCommands: {
-        default: 'npx nx run sample-angular:serve',
-        production: 'npx nx run sample-angular:serve-static',
-      },
-      ciWebServerCommand: 'npx nx run sample-angular:serve-static',
-      ciBaseUrl: 'http://localhost:4200',
-    }),
-    baseUrl: 'http://localhost:4200',
+    ...preset,
+    baseUrl: preset?.ciBaseUrl || 'http://localhost:4200', // Ensure baseUrl exists
   },
 });
